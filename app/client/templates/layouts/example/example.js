502933b341c157776afec263d392603f6dcc27c4
Pablo Padilla <pgpb.padilla@gmail.com>
2016-03-28 16:39:27 -0700

    new: usr: Add ReactiveDict to have a model in the example Template and update with the value from the query string when clicking the button.

--- app/client/templates/layouts/example/example.js
+++ app/client/templates/layouts/example/example.js
@@ -1,16 +1,26 @@
 /*****************************************************************************/
+
+import { ReactiveDict } from 'meteor/reactive-dict'
 /* Example: Event Handlers */
 /*****************************************************************************/
 Template.example.events({
+  'click #anAction' : (event, instance) => {
+    instance.myModelObj.set('rdSomeVar',
+      '**' + Iron.controller().state.get('someVar') + '**');
+  }
 });
 
 /*****************************************************************************/
 /* Example: Helpers */
 /*****************************************************************************/
 Template.example.helpers({
-  getSomeVar : () => {
+  getSomeVarFromController : () => {
     console.log('getSomeVar was called');
     return Iron.controller().state.get('someVar');
+  },
+  rdSomeVar () {
+    const self = Template.instance();
+    return 'This the value using a ReactiveDic = ' + self.myModelObj.get('rdSomeVar');
   }
 });
 
@@ -19,7 +29,9 @@ Template.example.helpers({
 /*****************************************************************************/
 Template.example.onCreated(function () {
   console.log('Example created');
-
+  this.myModelObj = new ReactiveDict();
+  this.myModelObj.set('p1', 'Some property that has no meaning');
+  this.myModelObj.set('rdSomeVar', 'Initial value for rdSomeVar');
 });
 
 Template.example.onRendered(function () {
