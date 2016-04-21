c88c18b29e622c8d4b332f36afc33866bc9b66a2
Pablo Padilla <pgpb.padilla@gmail.com>
2016-04-20 23:06:21 -0700

    new: dev: use twoLib from package one

--- app/packages/pgpb-one/client/one-widget.js
+++ app/packages/pgpb-one/client/one-widget.js
@@ -1,1 +1,9 @@
+import { twoLib } from 'meteor/pgpb:two';
 
+Template.oneWidget.events({
+    'click button' : (e) => {
+        console.log('twoLib', twoLib);
+        twoLib.makeTwo('from Package One: button click');
+        e.stopPropagation();
+    }
+});
